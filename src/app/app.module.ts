7cc277c83528867945ea2f31641efcb045f58db8
Jeremy Aguillon <agui1@umbc.edu>
2019-04-13 00:47:24 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import { DetailComponent } from './detail/detail.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreboardComponent,
+    DetailComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
